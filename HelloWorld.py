18ece789230062dc181ad3c03df9128b1e9a2ef5
unknown <eduard.soltuzu@QM-ESOLTUZU.qm.ro>
2015-05-28 09:37:39 +0300

    added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+#hello world test for GitHub
+
+import time
+
+#just a regular counter
+counter = 0
+
+while True:
+    print "{}. Hello World from Edewin!".format(counter)
+    time.sleep(1)
+    
