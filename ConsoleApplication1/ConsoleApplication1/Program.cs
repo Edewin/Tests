28898d59f467ca884bde2b9bcd2b1b5918f4ecf2
Edewin <eduard.soltuzu@outlook.com>
2016-01-25 11:41:34 +0200

    C# console App alloc memory into a method
    Passed by reference of object in C# allows to have full control on
    objects.
    
    In this example, allocation of memory is shown.
    
    Have fun!

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello C#!!!");
+
+            TestClass test = null;
+
+            allocMem( ref test);
+
+            Console.WriteLine( Environment.NewLine + "test.myMethod( 23, 2) returns: {0}", test.myMethod(23, 2) );
+
+
+            TestClass test2 = null;
+
+            allocMem(ref test2);
+
+            Console.WriteLine( Environment.NewLine + "test2.SimpleMethod() returns: {0}", test2.SimpleMethod() );
+
+            Console.ReadKey();
+
+        }
+
+        /******************************************************************/
+        // the main reason for making this little project is this method:
+        
+        static private void allocMem ( ref TestClass s)
+        {
+            s = new TestClass();
+        }
+
+        /******************************************************************/
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/TestClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TestClass
+    {
+        public TestClass()
+        {
+            this.MyInVar1 = 1;
+            this.MyInVar2 = 2;
+        }
+
+        // private:
+        private double MyInVar1;
+        private double MyInVar2;
+        private double MyOutVar;
+
+        // public:
+        public double getMyInVar1() { return this.MyInVar1; }
+        public double getMyInVar2() { return this.MyInVar2; }
+
+        public void setMyInVar1(double a) { this.MyInVar1 = a; }
+        public void setMyInVar2(double b) { this.MyInVar2 = b; }
+
+        public double SimpleMethod()
+        {
+            this.MyOutVar = this.MyInVar1 * this.MyInVar2;
+
+            return this.MyOutVar;
+        }
+
+        public double myMethod(double a, double b)
+        {
+            this.MyInVar1 = a;
+            this.MyInVar2 = b;
+
+            this.MyOutVar = a * b;
+
+            return this.MyOutVar;
+        }
+
+    }
+}
